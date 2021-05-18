9d6af3d5c91f6e5ea65b6fce7762e7c1752b422d
WeiJun <daniel.lin@senao.com>
2021-05-18 16:45:42 +0800

    Initial Commit

---
+++ CommonTool Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommonTool Demo
+//
+//  Created by WeiJun on 2021/5/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
